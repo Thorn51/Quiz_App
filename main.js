1cc9573ad509171ca0bc4a8403cdd8a646f1e085
Matt Thornberry <mtberry16@hotmail.com>
2019-07-16 11:02:59 -0600

    added function stubs and created question variable template

---
+++ main.js
@@ -0,0 +1,25 @@
+function createQuestion() {}
+
+function renderQuestion() {}
+
+function checkAnswer() {}
+
+function calculateScore() {}
+
+function rightAnswer() {}
+
+function wrongAnswer() {}
+
+function feedbackCorrect() {}
+
+function feedbackWrong() {}
+
+function renderResults() {}
+
+function nextQuestion() {}
+
+function restartQuiz() {}
+
+function createQuiz() {}
+
+$(createQuiz);
---
+++ quiz-question.js
@@ -0,0 +1,258 @@
+const QUESTIONS = [
+  {
+    id: jq0001,
+    question:
+      "Which jQuery object would you use to target all elements with the help-button class?",
+    exampleCode: null,
+    answers: [
+      {
+        answerOne: "$(‘#help-button’)",
+        correct: false
+      },
+      {
+        answerTwo: "(‘.help-button’)",
+        correct: false
+      },
+      {
+        answerThree: "$(‘.help-button’)",
+        correct: true
+      },
+      {
+        answerFour: "$(.help-button)",
+        correct: false
+      }
+    ],
+    feedback:
+      'jQuery(".myClass") - In this example all elements with the class .myClass would be selected. <a href="https://api.jquery.com/class-selector/"></a>'
+  },
+  {
+    id: jq0002,
+    question:
+      "Which jQuery object would you use to target all elements with the help-button class?",
+    exampleCode: null,
+    answers: [
+      {
+        answerOne: "$(‘#help-button’)",
+        correct: false
+      },
+      {
+        answerTwo: "(‘.help-button’)",
+        correct: false
+      },
+      {
+        answerThree: "$(‘.help-button’)",
+        correct: true
+      },
+      {
+        answerFour: "$(.help-button)",
+        correct: false
+      }
+    ],
+    feedback:
+      'jQuery(".myClass") - In this example all elements with the class .myClass would be selected. <a href="https://api.jquery.com/class-selector/"></a>'
+  },
+  {
+    id: jq0003,
+    question:
+      "Which jQuery object would you use to target all elements with the help-button class?",
+    exampleCode: null,
+    answers: [
+      {
+        answerOne: "$(‘#help-button’)",
+        correct: false
+      },
+      {
+        answerTwo: "(‘.help-button’)",
+        correct: false
+      },
+      {
+        answerThree: "$(‘.help-button’)",
+        correct: true
+      },
+      {
+        answerFour: "$(.help-button)",
+        correct: false
+      }
+    ]
+  },
+  {
+    id: jq0004,
+    question:
+      "Which jQuery object would you use to target all elements with the help-button class?",
+    exampleCode: null,
+    answers: [
+      {
+        answerOne: "$(‘#help-button’)",
+        correct: false
+      },
+      {
+        answerTwo: "(‘.help-button’)",
+        correct: false
+      },
+      {
+        answerThree: "$(‘.help-button’)",
+        correct: true
+      },
+      {
+        answerFour: "$(.help-button)",
+        correct: false
+      }
+    ],
+    feedback:
+      'jQuery(".myClass") - In this example all elements with the class .myClass would be selected. <a href="https://api.jquery.com/class-selector/"></a>'
+  },
+  {
+    id: jq0005,
+    question:
+      "Which jQuery object would you use to target all elements with the help-button class?",
+    exampleCode: null,
+    answers: [
+      {
+        answerOne: "$(‘#help-button’)",
+        correct: false
+      },
+      {
+        answerTwo: "(‘.help-button’)",
+        correct: false
+      },
+      {
+        answerThree: "$(‘.help-button’)",
+        correct: true
+      },
+      {
+        answerFour: "$(.help-button)",
+        correct: false
+      }
+    ],
+    feedback:
+      'jQuery(".myClass") - In this example all elements with the class .myClass would be selected. <a href="https://api.jquery.com/class-selector/"></a>'
+  },
+  {
+    id: jq0006,
+    question:
+      "Which jQuery object would you use to target all elements with the help-button class?",
+    exampleCode: null,
+    answers: [
+      {
+        answerOne: "$(‘#help-button’)",
+        correct: false
+      },
+      {
+        answerTwo: "(‘.help-button’)",
+        correct: false
+      },
+      {
+        answerThree: "$(‘.help-button’)",
+        correct: true
+      },
+      {
+        answerFour: "$(.help-button)",
+        correct: false
+      }
+    ],
+    feedback:
+      'jQuery(".myClass") - In this example all elements with the class .myClass would be selected. <a href="https://api.jquery.com/class-selector/"></a>'
+  },
+  {
+    id: jq0007,
+    question:
+      "Which jQuery object would you use to target all elements with the help-button class?",
+    exampleCode: null,
+    answers: [
+      {
+        answerOne: "$(‘#help-button’)",
+        correct: false
+      },
+      {
+        answerTwo: "(‘.help-button’)",
+        correct: false
+      },
+      {
+        answerThree: "$(‘.help-button’)",
+        correct: true
+      },
+      {
+        answerFour: "$(.help-button)",
+        correct: false
+      }
+    ],
+    feedback:
+      'jQuery(".myClass") - In this example all elements with the class .myClass would be selected. <a href="https://api.jquery.com/class-selector/"></a>'
+  },
+  {
+    id: jq0008,
+    question:
+      "Which jQuery object would you use to target all elements with the help-button class?",
+    exampleCode: null,
+    answers: [
+      {
+        answerOne: "$(‘#help-button’)",
+        correct: false
+      },
+      {
+        answerTwo: "(‘.help-button’)",
+        correct: false
+      },
+      {
+        answerThree: "$(‘.help-button’)",
+        correct: true
+      },
+      {
+        answerFour: "$(.help-button)",
+        correct: false
+      }
+    ],
+    feedback:
+      'jQuery(".myClass") - In this example all elements with the class .myClass would be selected. <a href="https://api.jquery.com/class-selector/"></a>'
+  },
+  {
+    id: jq0009,
+    question:
+      "Which jQuery object would you use to target all elements with the help-button class?",
+    exampleCode: null,
+    answers: [
+      {
+        answerOne: "$(‘#help-button’)",
+        correct: false
+      },
+      {
+        answerTwo: "(‘.help-button’)",
+        correct: false
+      },
+      {
+        answerThree: "$(‘.help-button’)",
+        correct: true
+      },
+      {
+        answerFour: "$(.help-button)",
+        correct: false
+      }
+    ]
+  },
+  {
+    id: jq0010,
+    question:
+      "Which jQuery object would you use to target all elements with the help-button class?",
+    exampleCode: null,
+    answers: [
+      {
+        answerOne: "$(‘#help-button’)",
+        correct: false
+      },
+      {
+        answerTwo: "(‘.help-button’)",
+        correct: false
+      },
+      {
+        answerThree: "$(‘.help-button’)",
+        correct: true
+      },
+      {
+        answerFour: "$(.help-button)",
+        correct: false
+      }
+    ],
+    feedback:
+      'jQuery(".myClass") - In this example all elements with the class .myClass would be selected. <a href="https://api.jquery.com/class-selector/"></a>'
+  }
+];
